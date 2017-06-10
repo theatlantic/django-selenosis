8f78fc519ffc49dd05c170fc7748d468d0960ea3
Frankie Dintino <fdintino@gmail.com>
2017-06-10 18:56:49 -0400

    Ignore a possible deprecation warning with python 3.4

--- django_admin_testutils/runtests.py
+++ django_admin_testutils/runtests.py
@@ -94,6 +94,10 @@ class RunTests(object):
             warnings.filterwarnings(
                 "ignore", "__class__ not set defining", DeprecationWarning)
 
+        # Ignore a deprecation warning in distutils.spawn for python 3.4
+        if sys.version_info[:2] == (3, 4):
+            warnings.filterwarnings("ignore", "the imp module", Warning)
+
         from django.conf import settings
 
         if 'grappelli' in settings.INSTALLED_APPS:
