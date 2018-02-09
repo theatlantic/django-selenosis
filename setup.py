821fbea8df0dbefc0ccb5c99202985e390b92fa0
Frankie Dintino <fdintino@gmail.com>
2018-02-09 11:21:01 -0500

    v1.0.7 [ci skip]

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(*parts):
 
 setup(
     name='django-admin-testutils',
-    version='1.0.6',
+    version='1.0.7',
     license='BSD',
     description='Helpers for writing selenium tests for the django admin',
     long_description=read('README.rst'),
