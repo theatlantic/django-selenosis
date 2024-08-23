5664113ba826c77f4b40065f96d044eb0a89cb03
Spencer Stantis <sirspen@gmail.com>
2024-08-23 16:38:37 -0500

    Put options behind environment variable

--- selenosis/selenium.py
+++ selenosis/selenium.py
@@ -127,12 +127,14 @@ class SelenosisTestCaseBase(type(LiveServerTestCase)):
             options.add_argument('headless')
             options.add_argument('disable-gui')
             options.add_argument('no-sandbox')
-            options.add_argument('disable-web-security')
-            options.add_argument('--disable-features=IsolateOrigins,site-per-process')
             if os.environ.get('DISABLE_DEV_SHM_USAGE'):
                 options.add_argument('disable-dev-shm-usage')
             if os.environ.get('CHROME_BIN'):
                 options.binary_location = os.environ['CHROME_BIN']
+            if os.environ.get('DISABLE_WEB_SECURITY'):
+                options.add_argument('disable-web-security')
+                options.add_argument('--disable-features=IsolateOrigins,site-per-process')
+
 
             kwargs['options'] = options
 
